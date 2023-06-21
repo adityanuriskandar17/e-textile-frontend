4ac93f0961f8eff55a516ab6f74a2fbd53fbdba6
aldijulfiansyah <aldiksk@gmail.com>
2023-06-21 20:35:06 +0700

    config dev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-4xl font-bold text-center my-8">
+      <h1 className="text-4xl font-bold text-center my-8 font-inter">
         Selamat datang di e-textile
       </h1>
       {/* Tambahkan komponen dan konten Anda di sini */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
     // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        pholder: "#A0A3BD", 
+        labeling: "#6E7191", 
+        succes: "#00BA88", 
+        eror: "#ED2E7E", 
+        btn1: "#00FF66", 
+        btn2: "#DB4444", 
+        bgSecondary: "#F5F5F5", 
+      },
+    },
   },
   plugins: [],
 };
